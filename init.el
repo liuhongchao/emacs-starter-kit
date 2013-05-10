ad01b5c37c0bdff414206918fd931390fe53fc06
hongchao <lbs.lhc@gmail.com>
2013-05-10 23:06:15 +0200

    add el-get

--- init.el
+++ init.el
@@ -99,6 +99,20 @@
       (insert "Welcome back Hongchao!\n"))
     (switch-to-buffer my-buffer)))
 
+;; ensure el-get is installed
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
+(el-get 'sync)
+
 ;; start the emacs server
 (server-start)
 
