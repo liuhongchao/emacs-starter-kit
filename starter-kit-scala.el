f1912d96a5cbd90a186df30ae021d5caaab33608
hongchao <hongchao@klarna.com>
2014-08-06 16:28:44 +0200

    add scala

---
+++ starter-kit-scala.el
@@ -0,0 +1,9 @@
+;;; starter-kit-scala.el --- Some helpful Scala helpers
+;;
+;; Part of the Emacs Starter Kit
+
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+(provide 'starter-kit-scala)
+;;; starter-kit-scala.el ends here
